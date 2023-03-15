3f5a873e6d61223e3e22d00261649468157c7c03
Claire-Trucy <trucy.claire92@gmail.com>
2023-03-15 10:56:11 +0100

    use emailjs for the contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,6 +1,21 @@
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import './styles.scss';
 
 export default function Contact() {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_msec5mj', 'template_cfvdpvp', form.current, 'EuiqluiunJNFkFV4m')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
+
     return (
         <div className="contact">
             <div className="contact_container">
@@ -8,11 +23,10 @@ export default function Contact() {
                     <h6>Vous êtes interessé(e)s?</h6>
                     <h3>Contactez-moi</h3>
                 </div>
-                <form action="#" method="post">
-                    <input type="text" name="name" placeholder="Entrez votre Nom ou Entreprise" required=""></input>
-                    <input type="email" name="email" placeholder="Entrez votre Email" required=""></input>
-                    <input type="text" name="object" placeholder="Entrez votre objet" required=""></input>
-                    <input type="text" name="number" placeholder="Entrez votre numéro de téléphone" required=""></input>
+                <form ref={form} onSubmit={sendEmail}>
+                    <input type="text" name="user_name" placeholder="Entrez votre Nom ou Entreprise" required=""></input>
+                    <input type="email" name="user_email" placeholder="Entrez votre Email" required=""></input>
+                    <input type="number" name="user_number" placeholder="Entrez votre numéro de téléphone" required=""></input>
                     <textarea name="message" placeholder="Entrez votre message"></textarea>
                     <button type="submit">Envoyer</button>
                 </form>
